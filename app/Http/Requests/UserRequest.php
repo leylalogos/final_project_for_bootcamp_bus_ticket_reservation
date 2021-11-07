f4bb93f73ff502a2f44675783b6a8bfeda3a6771
leyla sabouri <dev.leyla.sabouri@gmail.com>
2021-11-07 16:46:02 +0330

    complete user request

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\User;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'string',
+            'password' => 'required|min:6|confirmed',
+            'email' => 'required|email|unique',
+        ];
+    }
+}
