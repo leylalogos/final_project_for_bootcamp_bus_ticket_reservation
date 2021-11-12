1c87042c8378a17761b6ed1408733a0646f77f85
leyla sabouri <dev.leyla.sabouri@gmail.com>
2021-11-12 18:43:18 +0330

    validate bus fileds

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Bus;
+use App\Http\Requests\BusRequest;
 
 class BusController extends Controller
 {
-    public function store(Request $request)
+    public function store(BusRequest $request)
     {
         Bus::create([
             'capacity' => $request->capacity,
@@ -15,7 +16,7 @@ class BusController extends Controller
             'is_vip' => $request->is_vip,
             'user_id' => $request->user_id,
         ]);
-        return response()->json(array('message' => 'bus  successfully created'), 201);
+        return response()->json(array('message' => 'وسیله نقلیه شما با موفقیت اضافه شد.'), 201);
     }
 
     public function update(Request $request, Bus $bus)
@@ -23,11 +24,13 @@ class BusController extends Controller
 
         $bus->update($request->input());
 
-        return response()->json(array('message' => 'bus updated successfully'), 201);
+        return response()->json(array('message' => 'وسیله نقلیه شما با موفقیت بروز رسانی شد'), 204);
     }
+    
     public function destroy(Bus $bus)
     {
         $bus->delete();
-        return response()->json(array('message' => 'bus deleted successfully'), 201);
+
+        return response()->json(array('message' => 'مورد انتخابی با موفقیت حذف شد.'), 202);
     }
 }
--- app/Http/Requests/BusRequest.php
+++ app/Http/Requests/BusRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use App\Models\User;
 
-class UserRequest extends FormRequest
+class BusRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,9 +24,10 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'string',
-            'password' => 'required|string|min:6',
-            'email' => 'required|string|email|unique:users,email',
+            'capacity' =>'required|integer',
+            'name' =>'required|string',
+            'is_vip'=>'required|boolean',
+            'user_id' =>'required|integer'
         ];
     }
 }
