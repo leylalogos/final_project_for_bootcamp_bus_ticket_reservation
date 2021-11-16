c5e25f924c589c523d9dbe24fec67d7a2ea6ee31
leyla sabouri <dev.leyla.sabouri@gmail.com>
2021-11-16 12:06:32 +0330

    make seeder for comments

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Comment;
 
 class CommentSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comments = [
+            ['title' => 'شرکت ماهان', 'content' => 'سایت بسیار خوبیه من هیچ کونه مشکلی نداشتم', 'user_id' => 8],
+            ['title' => 'شرکت ماهان', 'content' => ' بخش پرداخت بسیار امن میباشد', 'user_id' => 8],
+            ['title' => 'شرکت سیر و سفر', 'content' => 'شرکت سیر سفر به عنوان مشتری قدیمی این شرکت می باشد', 'user_id' => 9],
+            ['title' => 'شرکت لوان', 'content' => '  لوان با مستر بلیط میتونه یه گزینه عالی باشه     ', 'user_id' => 10],
+
+        ];
+        if (Comment::count() == 0) {
+            foreach ($comments as $comment) {
+                Comment::create($comment);
+            }
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call([RoleSeeder::class]);
         $this->call([UserSeeder::class]);
         $this->call([PassportClientSeeder::class]);
-
+        $this->call([CommentSeeder::class]);
     }
 }
