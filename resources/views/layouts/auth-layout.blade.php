3da6cccc946827d933d627a31a7ef6ffb7de601c
leyla sabouri <dev.leyla.sabouri@gmail.com>
2021-11-24 22:25:44 +0330

    create login page with ajax

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -5,8 +5,16 @@
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>@yield('title')</title>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 </head>
 
 <body>
@@ -19,7 +27,7 @@
                 <div class="card card-body">
                     @yield('content')
                 </div>
-                
+
             </div>
         </div>
     </div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,12 +2,32 @@
 @section('title', 'صفحه ورود')
 @section('card-header-title', 'صفحه ورود')
 @section('content')
-    <form action="">
-        @csrf
-        @include('layouts.partials',['name'=>'email','type'=>'email','label'=>'ایمیل یا نام کاربری'])
-        @include('layouts.partials',['name'=>'password','type'=>'password','label'=>'رمز عبور'])
-        <div class="form-group  mt-3 ">
-            <input type="submit" value=" ورود" name="btn-register" class="btn btn-success">
-        </div>
-    </form>
+    @include('layouts.partials',['name'=>'email','type'=>'email','label'=>'ایمیل یا نام کاربری'])
+    @include('layouts.partials',['name'=>'password','type'=>'password','label'=>'رمز عبور'])
+    <div class="form-group  mt-3 ">
+        <button class="btn btn-success">ورود</button>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('.btn').click(function() {
+                $.ajax({
+                    type: 'POST',
+                    url: "{{ route('login') }}",
+                    data: {
+                        email: $('#email').val(),
+                        password: $('#password').val()
+                    },
+                    success: function(data) {
+                        if (data.access_token) {
+                            localStorage.setItem("access_token", data.access_token);
+                            console.log("login successful")
+                        } else {
+                            console.log(data.message);
+                        }
+                    }
+                })
+            })
+        });
+    </script>
 @stop
